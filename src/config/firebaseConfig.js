aa949ac7dce3fb902954b49431f170a825455957
Mac <mac@Macs-MacBook-Pro.local>
2023-11-04 11:51:17 +0500

    ---

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGD0CivbuhEkv7SsHeIHPR-4b8MtqhsYs",
-  authDomain: "jobslab-915a6.firebaseapp.com",
-  projectId: "jobslab-915a6",
-  storageBucket: "jobslab-915a6.appspot.com",
-  messagingSenderId: "69626704885",
-  appId: "1:69626704885:web:b85f5096ef47062f9aa6bc",
-  measurementId: "G-5J922X36VR",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
